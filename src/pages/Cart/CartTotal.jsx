74199c4e1c96982243c0fbab1d333cdba4fa2287
Recep Kececi <97679666+kececirecep@users.noreply.github.com>
2023-11-21 19:27:50 +0300

    Update CartTotal.jsx

--- src/pages/Cart/CartTotal.jsx
+++ src/pages/Cart/CartTotal.jsx
@@ -6,7 +6,7 @@ const CartTotal = () => {
         <h2 className='bg-[#E2F4FF] p-2 font-semibold text-center'>Cart total</h2>
         <div className='flex justify-between items-center p-12 pb-0 font-semibold text-[#232323]'>
             <p className=' text-sm'>Total amount</p>
-            <p className=' text-xl'>$23,20</p>
+            <p className=' text-xl'>$00,00</p>
         </div>
         <div className='flex flex-col p-12 pt-8 '>
             <button className='rounded-3xl bg-[#EDA415] text-white px-12 py-2 mb-4'>Continue</button>
